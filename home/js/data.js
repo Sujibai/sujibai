b25693ee7a6f048209ec8240483271718fb06f78
Sujibai <douniuzhijian@foxmail.com>
2019-01-31 23:39:32 +0800

    change site

--- home/js/data.js
+++ home/js/data.js
@@ -7,10 +7,14 @@ var maindata=[
         "icon":"icon-google",
         "url0":"https://www.google.com",
         "url1":"https://www.google.com.hk/search?q="},
-    {"id":"button-bing",//必应
-        "icon":"icon-bing",
-        "url0":"https://cn.bing.com",
-        "url1":"https://cn.bing.com/search?q="},
+    // {"id":"button-bing",//必应
+    //     "icon":"icon-bing",
+    //     "url0":"https://cn.bing.com",
+    //     "url1":"https://cn.bing.com/search?q="},
+    {"id":"button-miji",//秘迹
+        "icon":"icon-yan",
+        "url0":"https://mijisou.com",
+        "url1":"https://mijisou.com/?q="},
     // {"id":"button-weixin",//微信
     //     "icon":"icon-weixin",
     //     "url0":"http://weixin.sogou.com",
@@ -35,15 +39,12 @@ var maindata=[
         "icon":"icon-weibo",
         "url0":"http://weibo.com",
         "url1":"http://s.weibo.com/weibo/"},
-    // {"id":"button-tieba",//贴吧
-    //     "icon":"icon-tieba",
-    //     "url0":"https://tieba.baidu.com/index.html",
-    //     "url1":"http://tieba.baidu.com/f/search/res?ie=utf-8&qw="},
-    {"id":"button-leaf",//Overleaf
-        "icon":"icon-leaf",
-        "url0":"https://cn.overleaf.com/project"},
+    {"id":"button-tieba",//贴吧
+        "icon":"icon-tieba",
+        "url0":"https://tieba.baidu.com/index.html",
+        "url1":"http://tieba.baidu.com/f/search/res?ie=utf-8&qw="},
     {"id":"button-douban",//豆瓣
-        "icon":"icon-douzi",
+        "icon":"icon-douban",
         "url0":"https://www.douban.com/",
         "url1":"https://www.douban.com/search?cat=1002&q="},
     
@@ -51,23 +52,23 @@ var maindata=[
         "icon":"icon-tencenttv",
         "url0":"https://v.qq.com/",
         "url1":"https://v.qq.com/x/search/?q="},
-    {"id":"button-youku",//胖鸟
-        "icon":"icon-niao",
-        "url0":"http://www.pniao.com/",
-        "url1":"http://www.pniao.com/Mov/so/"},
+    {"id":"button-youku",//优酷
+        "icon":"icon-youku",
+        "url0":"http://www.youku.com/",
+        "url1":"http://www.soku.com/search_video/q_"},
     {"id":"button-baiduyun",//百度云
         "icon":"icon-baiduyun",
         "url0":"http://pan.baidu.com/disk/home",
         "url1":"http://www.xilinjie.com/s?q="},
     {"id":"button-youtube",//YouTube
-        "icon":"icon-youtube1",
+        "icon":"icon-youtube",
         "url0":"https://www.youtube.com/feed/subscriptions",
         "url1":"https://www.youtube.com/results?search_query="},
     {"id":"button-ustctv",//中科大网络电视
-        "icon":"icon-tv",
+        "icon":"icon-ustctv",
         "url0":"http://tv.byr.cn/show",},
     {"id":"button-github",//GitHub
-        "icon":"icon-github1",
+        "icon":"icon-github",
         "url0":"https://github.com/",
         "url1":"https://github.com/search?utf8=✓&q="},
     {"id":"button-byrbt",//北邮人
@@ -76,7 +77,7 @@ var maindata=[
         "url1":"http://bt.byr.cn/torrents.php?search=",},
     {"id":"button-whubt",//珞樱
         "icon":"icon-whubt",
-        "url0":"https://whu.pt/index.php",
-        "url1":"https://whu.pt/torrents.php?search="},
+        "url0":"https://pt.whu.edu.cn/",
+        "url1":"https://pt.whu.edu.cn/torrents.php?search="},
     
   ]
